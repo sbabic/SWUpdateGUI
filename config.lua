d3dea507fbb4f85d306c506ebf705f64d20f587d
Stefano Babic <sbabic@denx.de>
2019-10-08 16:56:36 +0200

    config.lua: add MEDIAPATH variable
    Signed-off-by: Stefano Babic <sbabic@denx.de>

--- config.lua
+++ config.lua
@@ -3,6 +3,7 @@ LOGO = "logo.png"
 NOREBOOT = true
 NETWORK_INTERFACES = {"eth0", "eth1"}
 MEDIA = "/media"
+MEDIAPATH = "/media"
 LANG= "de"
 STYLESHEETS = "industrial"
 function SAVEIPADDRESS(setup)
